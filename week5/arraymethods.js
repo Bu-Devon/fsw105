9f8c234bb90a8b3f5a582e5c078d04127aa44752
Bu-Devon <devon.brown@bryanuniversity.edu>
2021-01-26 23:13:28 -0800

    ez

--- week5/arraymethods.js
+++ week5/arraymethods.js
@@ -3,30 +3,31 @@ var vegtables = ["carrot", "tomato","pepper","lettuce"];
  
 function arrayChange(){
     vegtables.pop();
-    console.log ( "vegtables", vegtables);
+   
 
     fruit.shift();
-    console.log ("fruit", fruit);
+    
 
       var indexOfOrange = fruit.indexOf("orange");
-    console.log ("index of orange is:" + indexOfOrange);
+    
 
     fruit.push(indexOfOrange); 
-    console.log ("fruit", fruit);
+    
 
     vegtables.push(vegtables.length);
-    console.log ( "vegtables", vegtables);
    
-   var food = new Array (fruit.concat(vegtables));
-   console.log (food)
+   
+   var food = fruit.concat(vegtables);
+   
     
-    var removed = food.splice (4, 5);
-    console.log (removed)
-
-   var flat = food.shift();
-   console.log (flat)
-
-  var revr = food.reverse();
-     console.log  (revr)
+    var removed = food.splice (4,2) ;
+    
+    
+    var revr = food.reverse();
+    
+    
+      var div = food.toString();
+       
+       return div;
 }
 console.log (arrayChange());
